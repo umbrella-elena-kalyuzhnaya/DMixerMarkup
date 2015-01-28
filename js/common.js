18ea7d822a2d0fa425b06fdd6794ce22c8eaf3c3
elena.kalyuzhnaya <elena.kalyuzhnaya@umbrella-web.com>
2015-01-28 21:52:51 +0300

    Add modal window for search mobile

--- js/common.js
+++ js/common.js
@@ -1,146 +1,208 @@
 $(document).ready(function() {
 
-function isBreakPoint(bp) {  /* The breakpoints that you set in your css */
-    
-    var bps = [320, 480, 768, 1024, 1200, 2600];
-    var w = $(window).width();
-    var min, max;
-    for (var i = 0, l = bps.length; i < l; i++) {
-      if (bps[i] === bp) {
-        min = bps[i-1] || 0;
-        max = bps[i];
-        break;
-      }
+    function isBreakPoint(bp) { /* The breakpoints that you set in your css */
+
+        var bps = [320, 480, 768, 1024, 1200, 2600];
+        var w = $(window).width();
+        var min, max;
+        for (var i = 0, l = bps.length; i < l; i++) {
+            if (bps[i] === bp) {
+                min = bps[i - 1] || 0;
+                max = bps[i];
+                break;
+            }
+        }
+        return w > min && w <= max;
     }
-    return w > min && w <= max;
-  }
 
+    /* Padding fixed height Navbar with open Bootstrap dropdown */
 
-  $(window).on("resize", function () {
+    var animateNavbarHeight = function(additionalHeight) {
+        $('.dmixer-main-navbar').css({
+            paddingBottom: additionalHeight
+        });
+    }
 
-    if ( isBreakPoint(768) ) {  
-      /* Padding fixed height Navbar with open Bootstrap dropdown */
-        
-/*        var animateNavbarHeight = function(additionalHeight) {
-            $('.dmixer-main-navbar').css({
-                paddingBottom: additionalHeight
-                });
-        }
+    var removeHeaderBottom = function() {
+        if ($('.dmixer-menu-item').hasClass('open'))
+            animateNavbarHeight(0)
 
-        var removeHeaderBottom = function() {
-            if($('.dmixer-menu-item').hasClass('open'))
-                animateNavbarHeight(0)
+        if ($('.dmixer-search-item').hasClass('open'))
+            animateNavbarHeight(0)
+    }
 
-            if($('.dmixer-search-item').hasClass('open'))
-                animateNavbarHeight(0)
-        }
+    var addHeaderBottom = function(moreMenuHeight) {
+        if ($('.dmixer-menu-item').not('.open'))
+            animateNavbarHeight(moreMenuHeight)
+    }
 
-        var addHeaderBottom = function(moreMenuHeight) {
-            if($('.dmixer-menu-item').not('.open'))
-                animateNavbarHeight(moreMenuHeight)
-        }
+    var hideMainMenuElements = function() {
+      $('.dmixer-main-search .dropdown-toggle').on("click", (function(event) {
+        $('.dmixer-main-navbar').addClass('dmixer-collapsed-in');
+      }));
+    }
 
-        $('body').on("click",function() {
+    var addDropdownHeight = function(mobileMenuHeight) {
+        $('body').on("click", function() {
             removeHeaderBottom();
         });
 
-        $('.dmixer-main-menu .dropdown-toggle').on("click",(function(event){
-            /*event.stopPropagation();
-            var moreMenuHeight = $('.more-menu').height();
+        $('.dmixer-main-menu .dropdown-toggle').on("click", (function(event) {
+            /*event.stopPropagation();*/
+            var moreMenuHeight = $('.dmixer-more-menu').height();
             addHeaderBottom(moreMenuHeight);
 
-           }));
+        }));
 
-        $('.dmixer-main-search .dropdown-toggle').on("click",(function(event){
-            /*event.stopPropagation();
-            var moreMenuHeight = $('.dmixer-main-search .more-menu').height();
+        $('.dmixer-main-search .dropdown-toggle').on("click", (function(event) {
+            /*event.stopPropagation();*/
+            var moreMenuHeight = $('.dmixer-main-search .dmixer-more-menu').height();
+            var moreMenuPaddingTop = parseInt($('.dmixer-main-search .dmixer-more-menu').css('padding-top'));
+            var moreMenuPaddingBottom = parseInt($('.dmixer-main-search .dmixer-more-menu').css('padding-bottom'));
+
+            moreMenuHeight = moreMenuHeight + moreMenuPaddingTop + moreMenuPaddingBottom + mobileMenuHeight;
             addHeaderBottom(moreMenuHeight);
+            var collapsedBarHeight = $(".navbar-collapse").height();
+            $(".dmixer-main-search.navbar-nav .dropdown-menu").css("top", collapsedBarHeight);
 
-           }));*/
-
+        }));
     }
 
-  }).resize();
+    var preventScrollBgMobile = function() {
+        $('body').css({
+              'overflow':'hidden'
 
-/*  var carouselContent = $('.post-item').parent().html();
-  var postCircularCount =$(".post-circular li").length;
-  console.log(postCircularCount);
-  function isBreakPoint(bp) {  /* The breakpoints that you set in your css  
-    
-    var bps = [320, 480, 768, 1024, 1200, 2600];
-    var w = $(window).width();
-    var min, max;
-    for (var i = 0, l = bps.length; i < l; i++) {
-      if (bps[i] === bp) {
-        min = bps[i-1] || 0;
-        max = bps[i];
-        break;
+            });
+        /*$('#dmixer-navbar-collapsed').on("click",function(){
+          
+          if ($('#dmixer-navbar-collapsed').hasClass('collapse')) console.log('test');
+          
+        });*/
+        
+    }
+
+
+    $(window).on("resize", function() {
+
+        if (isBreakPoint(320)) {
+          $("#button-modal").click(function(){
+              Show("modal-id");
+          });
+          preventScrollBgMobile();
+        }
+
+        if (isBreakPoint(480)) {
+          $("#button-modal").click(function(){
+              Show("modal-id");
+          });
+          preventScrollBgMobile();
+        }
+        if (isBreakPoint(768)) {
+          $("#button-modal").click(function(){
+              Show("modal-id");
+          });
+          preventScrollBgMobile();
+        }
+        
+        if (isBreakPoint(1024)) {
+            addDropdownHeight(0);
+        }
+        
+        if (isBreakPoint(1200)) {
+            addDropdownHeight(0);
+        }
+
+        if (isBreakPoint(2600)) {
+            addDropdownHeight(0);
+        }
+
+    }).resize();
+
+//Fix modal mobile Boostrap 3
+function Show(id){
+  //Fix CSS
+  $(".modal-footer").css({"padding":"19px 20px 20px","margin-top":"15px","text-align":"right","border-top":"1px solid #e5e5e5"});
+  $(".modal-body").css("overflow-y","auto");
+  //Fix .modal-body height
+  $('#'+id).on('shown.bs.modal',function(){
+    $("#"+id+">.modal-dialog>.modal-content>.modal-body").css("height","auto");
+    h1=$("#"+id+">.modal-dialog").height();
+    h2=$(window).height();
+    h3=$("#"+id+">.modal-dialog>.modal-content>.modal-body").height();
+    h4=h2-(h1-h3);    
+    if($(window).width()>=768){
+      if(h1>h2){
+        $("#"+id+">.modal-dialog>.modal-content>.modal-body").height(h4);
+      }
+      $("#"+id+">.modal-dialog").css("margin","30px auto");
+      $("#"+id+">.modal-dialog>.modal-content").css("border","1px solid rgba(0,0,0,0.2)");
+      $("#"+id+">.modal-dialog>.modal-content").css("border-radius",6);       
+      if($("#"+id+">.modal-dialog").height()+30>h2){
+        $("#"+id+">.modal-dialog").css("margin-top","0px");
+        $("#"+id+">.modal-dialog").css("margin-bottom","0px");
       }
     }
-    return w > min && w <= max;
-  }
-
-  
-  var carouFredSelCall = function () {    /*  Custom configuration carousel  
- 
-    var options = {
-          items               : 2,
-          direction           : "left",
-          responsive          : "true",
-          prev: {
-              button: "#prev",
-              key: "left"
-          },
-          next: {
-              button: "#next",
-              key: "right"
-          },
-          scroll : {
-              play            : false,
-              items           : 1,
-              easing          : "quadratic",
-              duration        : 1000,                         
-              pauseOnHover    : true
-          }                   
+    else{
+      //Fix full-screen in mobiles
+      $("#"+id+">.modal-dialog>.modal-content>.modal-body").height(h4);
+      $("#"+id+">.modal-dialog").css("margin",0);
+      $("#"+id+">.modal-dialog>.modal-content").css("border",0);
+      $("#"+id+">.modal-dialog>.modal-content").css("border-radius",0); 
+    }
+    //Aply changes on screen resize (example: mobile orientation)
+    window.onresize=function(){
+      $("#"+id+">.modal-dialog>.modal-content>.modal-body").css("height","auto");
+      h1=$("#"+id+">.modal-dialog").height();
+      h2=$(window).height();
+      h3=$("#"+id+">.modal-dialog>.modal-content>.modal-body").height();
+      h4=h2-(h1-h3);
+      if($(window).width()>=768){
+        if(h1>h2){
+          $("#"+id+">.modal-dialog>.modal-content>.modal-body").height(h4);
+        }
+        $("#"+id+">.modal-dialog").css("margin","30px auto");
+        $("#"+id+">.modal-dialog>.modal-content").css("border","1px solid rgba(0,0,0,0.2)");
+        $("#"+id+">.modal-dialog>.modal-content").css("border-radius",6);       
+        if($("#"+id+">.modal-dialog").height()+30>h2){
+          $("#"+id+">.modal-dialog").css("margin-top","0px");
+          $("#"+id+">.modal-dialog").css("margin-bottom","0px");
+        }
+      }
+      else{
+        //Fix full-screen in mobiles
+        $("#"+id+">.modal-dialog>.modal-content>.modal-body").height(h4);
+        $("#"+id+">.modal-dialog").css("margin",0);
+        $("#"+id+">.modal-dialog>.modal-content").css("border",0);
+        $("#"+id+">.modal-dialog>.modal-content").css("border-radius",0); 
+      }
     };
-
-    $('#carousel ul').carouFredSel(options);
-
-  }
-  var carouselContent = $('.post-circular').parent().html();
-
-  $(window).on("resize", function () {
-
-    if ( isBreakPoint(2600) ) {  /* Carousel initialize 
-
-      carouFredSelCall();
-
-    }
-    if ( isBreakPoint(1024) ) {  /* Carousel destroy  
-
-      $('#carousel').empty().html(carouselContent);  
-
-      if (postCircularCount >=3) {
-        var postHeight = parseInt($('.post-item').height()) + parseInt($('.post-item').css('margin-bottom'));
-        console.log(postHeight*3);
-      } 
-      else {
-        $(this).find("view-more").addClass('hide-block')
-      }
-
-      
-    }
-
-  }).resize();*/
-
-
-$(".dmixer-title-centered-und").each(function(){
-  var titleWidth = $(this).width();
-  $(this).append('<span class="temp" style="width:'+ titleWidth +'px"></span>');
-});
-
-
-
+  });  
+  //Free event listener
+  $('#'+id).on('hide.bs.modal',function(){
+    window.onresize=function(){};
+  });  
+  //Mobile haven't scrollbar, so this is touch event scrollbar implementation
+  var y1=0;
+  var y2=0;
+  var div=$("#"+id+">.modal-dialog>.modal-content>.modal-body")[0];
+  div.addEventListener("touchstart",function(event){
+    y1=event.touches[0].clientY;
+  });
+  div.addEventListener("touchmove",function(event){
+    event.preventDefault();
+    y2=event.touches[0].clientY;
+    var limite=div.scrollHeight-div.clientHeight;
+    var diff=div.scrollTop+y1-y2;
+    if(diff<0)diff=0;
+    if(diff>limite)diff=limite;
+    div.scrollTop=diff;
+    y1=y2;
+  });
+  //Fix position modal, scroll to top.  
+  $('html, body').scrollTop(0);
+  //Show
+  $("#"+id).modal('show');
+}
 
 
 });
